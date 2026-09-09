7a5824714cda3e84451232f841c5351e56523bad
chmajster <44300091+chmajster@users.noreply.github.com>
2026-09-09 03:22:17 +0200

    fix(app): add required IO and HTTP namespaces

--- src/DevBox.App/MainWindow.xaml.cs
+++ src/DevBox.App/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
