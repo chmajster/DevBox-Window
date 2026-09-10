b5db75b2be787ac923d29b86e01838db0a936e28
chmajster <44300091+chmajster@users.noreply.github.com>
2026-09-10 02:19:54 +0200

    feat: open project manager from tools

--- src/DevBox.App/ToolsWindow.xaml.cs
+++ src/DevBox.App/ToolsWindow.xaml.cs
@@ -1,14 +1,20 @@
 using System.Windows;
+using DevBox.App.Services;
 using DevBox.App.ViewModels;
 
 namespace DevBox.App;
 
 public partial class ToolsWindow : Window
 {
-    public ToolsWindow(ToolsWindowViewModel viewModel)
+    private readonly IFeatureWindowService _featureWindows;
+
+    public ToolsWindow(ToolsWindowViewModel viewModel, IFeatureWindowService featureWindows)
     {
         InitializeComponent();
         DataContext = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
+        _featureWindows = featureWindows ?? throw new ArgumentNullException(nameof(featureWindows));
         Loaded += async (_, _) => await viewModel.RefreshAsync();
     }
+
+    private void ProjectManager_Click(object sender, RoutedEventArgs e) => _featureWindows.ShowProjects(this);
 }
